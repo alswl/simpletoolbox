53575d504cbe61e7586d3464968444eb51d8b239
alswl <alswlx@gmail.com>
2011-11-02 21:09:50 +0800

    增加WP-EasyArchives和标签云Page页面模板

---
+++ archives.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: WP-EasyArchives
+ * Description: For plugin WP-EasyArchives
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'easyarchives' ); ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ content-easyarchives.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template used for displaying page content in archives.php
+ * Description: For plugin WP-EasyArchives
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php wp_easyarchives(); ?>
+
+		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
+		<?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="edit-link">', '</span>' ); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
